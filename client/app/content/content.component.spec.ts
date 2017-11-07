3a3f542f1b67d285286e027b27f7c810b9121fd5
ssi-bsuralta <Bryan.SURALTA@SurveySampling.com>
2017-11-07 12:37:21 -0500

    test pass

--- client/app/content/content.component.spec.ts
+++ client/app/content/content.component.spec.ts
@@ -1,19 +1,29 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ContentComponent } from './content.component';
 import { SharedModule } from '../shared/shared.module';
 
 import { HeaderComponent } from '../layout/header/header.component';
 import { MenuComponent } from '../layout/menu/menu.component';
 
+import { AuthService } from '../login/services/auth.service';
+import { mockAuth } from '../login/services/auth.mock';
+
 describe('ContentComponent', () => {
     beforeEach(async(() => {
         TestBed
             .configureTestingModule({
-                imports: [SharedModule],
+                imports: [
+                    SharedModule,
+                    RouterTestingModule
+                ],
                 declarations: [
                     ContentComponent,
                     HeaderComponent,
                     MenuComponent
+                ],
+                providers: [
+                    { provide: AuthService, useValue: mockAuth }
                 ]
             })
             .compileComponents();
