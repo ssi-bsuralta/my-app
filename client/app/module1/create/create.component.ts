bb549da9ef0e5032fd385d28b7d60de2b03c7e08
ssi-bsuralta <Bryan.SURALTA@SurveySampling.com>
2017-11-08 19:20:08 -0500

    include jquery

--- client/app/module1/create/create.component.ts
+++ client/app/module1/create/create.component.ts
@@ -1,12 +1,19 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { slideInDownAnimation } from '../../shared/animations';
+
+declare const $;
 
 @Component({
     templateUrl: './create.component.html',
     styleUrls: ['./create.component.scss'],
     animations: [slideInDownAnimation]
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
     @HostBinding('@routeAnimation') routeAnimation = true;
     @HostBinding('style.display') display = 'block';
+
+    ngOnInit() {
+        $('#sortable').sortable();
+        $('#sortable').disableSelection();
+    }
 }
